860ea0084ab442b0b2b0c9b256276f27511070c7
DEUS <perevyshink_1@mail.ru>
2020-03-17 14:01:35 +0600

    polishing

--- app/src/main/java/com/dbuster/cscalculator/AboutActivity.java
+++ app/src/main/java/com/dbuster/cscalculator/AboutActivity.java
@@ -2,22 +2,71 @@ package com.dbuster.cscalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
-
+    private int count = 0;
+    private long startMillis=0;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ImageView icon = findViewById(R.id.icon_aboutpage);
         final TextView githubTV = findViewById(R.id.githubrepoTV);
         final TextView telegramTV = findViewById(R.id.telegramTV);
+        icon.setOnTouchListener(new View.OnTouchListener() {
+            Handler handler = new Handler();
+
+            int numberOfTaps = 0;
+            long lastTapTimeMs = 0;
+            long touchDownMs = 0;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        touchDownMs = System.currentTimeMillis();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        handler.removeCallbacksAndMessages(null);
+
+                        if ((System.currentTimeMillis() - touchDownMs) > ViewConfiguration.getTapTimeout()) {
+                            //it was not a tap
+
+                            numberOfTaps = 0;
+                            lastTapTimeMs = 0;
+                            break;
+                        }
+
+                        if (numberOfTaps > 0
+                                && (System.currentTimeMillis() - lastTapTimeMs) < ViewConfiguration.getDoubleTapTimeout()) {
+                            numberOfTaps += 1;
+                        } else {
+                            numberOfTaps = 1;
+                        }
+
+                        lastTapTimeMs = System.currentTimeMillis();
+
+                        if (numberOfTaps == 3) {
+                            Toast.makeText(getApplicationContext(), "No easter eggs over here :(", Toast.LENGTH_SHORT).show();
+                        }
+                }
+                return true;
+            }
+        });
+
         githubTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
